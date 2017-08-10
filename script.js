89f9a5f621b1dd6e7f9aa063e5beb617bb5fa34f
Frank Nardone <fnardone@albany.edu>
2017-08-10 13:42:59 -0400

    Added navbar animation, added a javascript file with a function that scrolls page when clicking on different pages from the navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("ul.nav li a[href^='#']").click(function () {
+        $("html, body").stop().animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        }, 400);
+    });
+});
